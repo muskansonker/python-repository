eab9b344b6cdf3a55b4c7ca47970b4cc2a2c40a2
muskan sonker <muskansonker022@gmail.com>
2022-12-10 16:57:29 +0530

    Questions

---
+++ strings/string_Question.py
@@ -0,0 +1,157 @@
+# Questions
+
+# ? Ques-1 
+
+string1= "Hello Universe"
+#print(string1)
+
+# ? Ques-2 
+
+#s=input()
+#print(len(s))
+
+# ? Ques-3 
+
+a="Python is great"
+#print(a[10:])
+
+# ? Ques-4
+
+b="Python is everywhere"
+b2=b.replace(' ', '\n')
+print(b2)
+
+# ? Ques-5
+
+c="Hello World"
+print(c[::-1]) #means take -1 step from backward 
+
+# ? Ques-6
+
+e="How are you?"
+print(e.upper())
+
+# ? Ques-7
+
+f="How Is It Going?"
+print(f.lower())
+
+# ? Ques-8
+
+words=['Python','is','easy','to','learn']
+for i in words:
+    print(i,end=' ')
+
+# ? Ques-9
+
+multi='''
+Python is a high-level, general-purpose
+programming language. Its design
+philosophy emphasizes code 
+readability with the use of significant indentation. 
+Python is dynamically-typed and garbage-collected. 
+It supports multiple programming paradigms, 
+including structured, object-oriented and functional 
+programming. 
+'''
+#print(multi)
+
+# ? Ques-10
+
+string2="to move to newline '\n' is used."
+print(string2)
+
+# ? Ques-11
+
+var1=" the variable is 15"
+print(var1)
+
+# ? Ques-12
+
+s1='Python'
+s2='is'
+s3='great'
+s4=s1+" "+s2+" "+s3
+print(s4)
+
+# ? Ques-13
+
+print('#'*20)
+
+# ? Ques-14
+
+#for i in range(1,10):
+    #print(f'{i}. ')
+
+# ? Ques-15
+
+#i=input("Enter a sentence: ")
+#print(i.replace(' ', '\n'))
+
+# ? Ques-16
+
+#inp=input("Enter a String: ")
+#if(inp.endswith('?')):
+    #print("hi")
+
+# ? Ques-17
+
+#ip=input("Enter string: ")
+#print(ip.count('e'))
+
+# ? Ques-18
+
+#ip=input("Enter anything ")
+#op=ip.isnumeric()
+#if op==1:
+    #print("its numeric")
+#else:
+    #print("its string")
+
+# ? Ques-19 **PB
+
+#text="   this is not a good string   "
+#if text.startswith('   ') & text.endswith('   '):
+    
+# ? Ques-20  ** PB
+
+#ip=input("Enter string- ")
+#if ip.isupper():
+    #print("Found")
+#else:
+    #print("no")
+
+# ? Ques-21  **PB
+
+#names='Joe, David, Mark, Tom, Chris, Robert'
+#m=[]
+#for i in names:
+    #m.extend(i)
+#m=names.copy()
+#print(m)
+
+# ? Ques-22   **PB
+
+text='this is some text'
+print()
+
+# ? Ques-23
+
+#ip=input("Enter string- ")
+#idx=ip.find('fyi')
+#if idx!=1:
+    #print(f'Found "fyi" at index {idx}')
+#else:
+    #print('Not Found "fyi"')
+
+# ? Ques-24       **PB
+
+test='%p34@y!*-*!t68h#&on404'
+for i in test:
+    if i.isalpha():
+        print(i)
+print(test)
+
+
+# ? Ques-25      **PB
+
