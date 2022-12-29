86271ab87bf0bd995fdcf5396d5236a0315ffb5a
muskan sonker <muskansonker022@gmail.com>
2022-12-29 23:37:47 +0530

    Update g2.py

--- funcs/g2.py
+++ funcs/g2.py
@@ -2,10 +2,12 @@ import pgzrun
 WIDTH=800
 HEIGHT=500
 
+#! Taking block size on screen(800*500)
 square=Rect((400,250),(50,50))
 s2=Rect((200,250),(50,50))
 
 b_vx=4 #! global vairable
+#* b_vx is variable for speed... 4 is the speed of b_vx.
 
 def draw():
     screen.clear()
@@ -14,10 +16,13 @@ def draw():
 
 def update():
     global b_vx #* this allows us to change the value of b_vx
+    
     #! loop around the screen
-    square.x+=2
-    if square.x>WIDTH:
+    square.x+=2 #* square variable will add 2(speed) or called the speed on x axis.
+    if square.x>WIDTH: #* square variable on x axis is greater than width i.e., 800
+        #* when the condition is true do this -->
         square.x=0
+    
     #! bounce the box
     s2.x+=b_vx
     if s2.right>WIDTH or s2.left<0:
