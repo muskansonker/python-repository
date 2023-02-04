c409c03f6090687f26cc8bd00ed81e5b54afa5c2
muskan sonker <muskansonker022@gmail.com>
2023-02-04 20:00:58 +0530

    Revert "dictionary"
    This reverts commit eb300813d5abfb53f57c6f83472ba225d8be0611.

--- strings/string_Question.py
+++ strings/string_Question.py
@@ -19,57 +19,52 @@ a="Python is great"
 
 b="Python is everywhere"
 b2=b.replace(' ', '\n')
-#print(b2)
+print(b2)
 
 # ? Ques-5
 
 c="Hello World"
-#print(c[::-1]) #means take -1 step from backward 
+print(c[::-1]) #means take -1 step from backward 
 
 # ? Ques-6
 
 e="How are you?"
-#print(e.upper())
+print(e.upper())
 
 # ? Ques-7
 
 f="How Is It Going?"
-#print(f.lower())
+print(f.lower())
 
 # ? Ques-8
 
 words=['Python','is','easy','to','learn']
 for i in words:
-    #print(i,end=' ')
-# ! ALTERNATE WAY
-#sentence = ' '.join(words)
-#print(sentence)
+    print(i,end=' ')
 
 # ? Ques-9
 
-#multi='''
-#Python is a high-level, general-purpose
-#programming language. Its design
-#philosophy emphasizes code 
-#readability with the use of significant indentation. 
-#Python is dynamically-typed and garbage-collected. 
-#It supports multiple programming paradigms, 
-#including structured, object-oriented and functional 
-#programming. 
-#'''
+multi='''
+Python is a high-level, general-purpose
+programming language. Its design
+philosophy emphasizes code 
+readability with the use of significant indentation. 
+Python is dynamically-typed and garbage-collected. 
+It supports multiple programming paradigms, 
+including structured, object-oriented and functional 
+programming. 
+'''
 #print(multi)
 
 # ? Ques-10
 
-#string2="to move to newline '\n' is used."
-#print(r'string2') #raw string gives as it is written in question.
-# ! ALTERNATE WAY
-#print("to move to newline '\\n' is used")
+string2="to move to newline '\n' is used."
+print(r'string2') #raw string gives as it is written in question.
 
 # ? Ques-11
 
 var1=" the variable is 15"
-#print(var1)
+print(var1)
 
 # ? Ques-12
 
@@ -77,11 +72,11 @@ s1='Python'
 s2='is'
 s3='great'
 s4=s1+" "+s2+" "+s3
-#print(s4)
+print(s4)
 
 # ? Ques-13
 
-#print('#'*20)
+print('#'*20)
 
 # ? Ques-14
 
@@ -115,40 +110,32 @@ s4=s1+" "+s2+" "+s3
 
 # ? Ques-19 **PB
 
-text="   this is not a good string   "
-text2=" "
-for i in text.split():
-    if i==' ':
-        i+1
-
+#text="   this is not a good string   "
+#if text.startswith('   ') & text.endswith('   '):
     
-
-    
-# ? Ques-20  **PB**
+# ? Ques-20  ** PB
 
 #ip=input("Enter string- ")
-#found=0
 #for i in ip:
-   #if ip.isupper():
+    #found=0
+#   if ip.isupper():
         #found+=1
-#if found:
-    #print("Found")
-    #print(f'uppercase letter occurred {found} times')
+        #print("Found")
     
 
-# ? Ques-21  **PB**
+# ? Ques-21  **PB
 
 #names='Joe, David, Mark, Tom, Chris, Robert'
-#list2=names.split(', ')
-#print(list2)
+#m=[]
+#for i in names:
+    #m.extend(i)
+#m=names.copy()
+#print(m)
 
-# ? Ques-22   **PB**
+# ? Ques-22   **PB
 
 #text='this is some text'
-#output = ''
-#for word in text.split():
-    #output += f'{word}aye '
-#print(output)
+#print()
 
 # ? Ques-23
 
@@ -159,12 +146,12 @@ for i in text.split():
 #else:
     #print('Not Found "fyi"')
 
-# ? Ques-24       **PB**
+# ? Ques-24       **PB
 
 test='%p34@y!*-*!t68h#&on404'
-#for i in test:
-    #if i.isalpha():
-        #print(i,end='')
+for i in test:
+    if test[i].isalpha:
+        print(i)
 
 
 
